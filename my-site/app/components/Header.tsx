e156e39fa5cb5b5a437409a11a23f616d58a93d3
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-07-29 07:15:24 -0400

    playing with next 13

--- my-site/app/components/Header.tsx
+++ my-site/app/components/Header.tsx
@@ -1,13 +1,16 @@
-'use client'
-
+import { delay } from "../lib/delay";
 import HeaderLink from "./HeaderLink";
 
-const Header = () => {
+const Header = async () => {
+    await delay(5000)
     return (
         <header>
+            logo
+            <div>
             <HeaderLink />
+            </div>
         </header>
-    );
+    )
     }
 
 export default Header;
